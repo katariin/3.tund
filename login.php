dfc30d1901767915e7332b76861811ad35fcfad1
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-09-18 11:08:00 +0300

    3_tund

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+
+//echo $_POST["email"];
+
+//Defineerime muutujad
+$email_error = "";
+$password_error = "";
+
+//kontrollin kas keegi vajutas nuppu
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+   //echo "jah";
+   
+   //kas e-post on tühi
+   if( empty($_POST["email"]) ) {
+	   
+	   //jah oli tühi
+	   $email_error = "See Väli on kohustuslik";
+	   
+   }
+}
+
+?>
+<html>
+<head>
+<title>Login page</title>
+</head>
+
+<body>
+<h2>Login</h2>
+<form action="login.php" method="post">
+<input type="email" placeholder="E-post"><br><br>
+<input type="password" placeholder="Parool"><br><br>
+<input type="submit" value="Logi sisse"><br><br>
+
+<h2>Create User</h2>
+
+</body>
