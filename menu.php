75dc3c641b5219d85c16fac168d4eea475650e47
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-09-18 12:45:45 +0300

    .

--- menu.php
+++ menu.php
@@ -1,11 +1,26 @@
 <h3>menüü</h3>
-<?php echo $file_name; ?>
+<?php //echo $file_name; ?>
 <ul>
-    <li>
-	<a href="home.php">Avaleht</a>
-	</li>
-	</ul>
+
+   <?php if($file_name == "home.php"){ ?>
+   
+    <li>Avaleht</li>
+	
+   <?php } else ] { ?>
+   
+	
 	<li>
 	<a href="home.php">Logi_sisse</a>
 	</li>
-	</ul>
+	
+   <?php } ?>
+   
+   <?php
+   
+   if($file_name == "login.php") {
+	    echo "<li>Logi sisse</li>";
+		
+   }else{
+         echo '<li><a href ="login.php">Logi sisse</li>'   
+   }
+   ?>
